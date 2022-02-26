ad1ddd2ab0122fc38ca31975e8e282dc4d6cb293
Beata Karwowska <bsz3636@o2.pl>
2022-02-26 15:27:39 +0100

    React-first reposytory

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'font-awesome/css/font-awesome.min.css';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
